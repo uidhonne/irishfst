19d2866b495e375849fdde06b802e8e3f709504f
Micheal Johnny <michealomeachair@gmail.com>
2017-08-02 15:36:00 +0100

    Minor changes and note about utf-8 tokenization

--- test.sh
+++ test.sh
@@ -6,11 +6,13 @@
 # 4. vislcg3  disambiguation
 # elaine oct 2012
 
-# SCRAP
+# This isn't utf-8 aware (deleted áóúéíÁÓÚÉÍ during tokenization)
 # tr -sc "[:alnum:]" "[\n*]"
-#  | perl $HOME/irish-pos-install/tok/tokclean.prl | flookup -a bin/lexguess.fst | perl dis/lookup2cg3.prl | vislcg3 -g dis/gael-dis.rle
 
-cat Logainm.txt |  tokenize -utf8 -d 700 $HOME/irish-pos-install/tok/tok-gael.fst | flookup -a bin/lexguess.fst | perl dis/lookup2cg3.prl | vislcg3 -g dis/gael-dis.rle > LogainmPOS.txt
+# This part of the pipeline is not always used because of RAM limitations
+# | perl $HOME/irish-pos-install/tok/tokclean.prl |
+
+cat Logainm.txt | tokenize -utf8 -d 700 $HOME/irish-pos-install/tok/tok-gael.fst | flookup -a bin/lexguess.fst | perl dis/lookup2cg3.prl | vislcg3 -g dis/gael-dis.rle > LogainmPOS.txt
 
 
 
